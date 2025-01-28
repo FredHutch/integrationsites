7322c0837a012d15d368f749adb2c334622aa79e
Wenjie Deng <wdeng2@dr62vgc64p.fhcrc.org>
2025-01-28 10:41:18 -0800

    modified extractGenesFromGffFile.py to include region info

--- cgi-bin/extractGenesFromGffFile.py
+++ cgi-bin/extractGenesFromGffFile.py
@@ -9,16 +9,19 @@ if len(sys.argv) < 3:
 
 inFile = sys.argv[1]
 outFile = sys.argv[2]
-count = 0
+gcount, rcount = 0, 0
 
 with open(inFile, 'r') as in_file, open(outFile, 'w') as out_file:
-        for line in in_file:
-            if line.startswith('#'):
-                continue
-            fields = line.strip().split('\t')
-            chromo, source, feature = fields[0], fields[1], fields[2]
-            if feature == "gene" or feature == "pseudogene":
-                out_file.write(line)
-                count += 1
+    for line in in_file:
+        if line.startswith('#'):
+            continue
+        fields = line.strip().split('\t')
+        chromo, source, feature = fields[0], fields[1], fields[2]
+        if feature == "region":
+            out_file.write(line)
+            rcount += 1
+        elif feature == "gene" or feature == "pseudogene":
+            out_file.write(line)
+            gcount += 1
 
-print(f"Total {count} genes and pseudogene.")
+print(f"Total {rcount} regions, {gcount} genes/pseudogene.")
